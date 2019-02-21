d64ba7d0c06b3078e2c6d96611957cd89f5be075
Alexander Bartolomey <occloxium@gmail.com>
2019-02-21 13:32:31 +0100

    export complications in DOMHelper fixed

--- DOMHelper.js
+++ DOMHelper.js
@@ -2,27 +2,25 @@
  * The default transition time for fades
  * This does not actually affect any animation, it is only used
  * for asynchronous triggers to affect element classes
- * @property {number} [__transitionTime=500] transition time
+ * @property {number} [TRANSITION_TIME=500] transition time
  */
-let __transitionTime = 500;
+export let TRANSITION_TIME = 500;
 
 export class DOMHelper {
-  constructor() {
-  }
   /**
    * @returns {number} the default transition time for fades
    */
-  getTransitionTime() {
-    return __transitionTime();
+  static getTransitionTime() {
+    return TRANSITION_TIME;
   }
   /**
    * Sets the default transition time
    * @param {number} [time=500] time must be less than 1000ms
    */
-  setTransitionTime(time) {
+  static setTransitionTime(time) {
     return (typeof time === 'number' && time <= 1000
-      ? __transitionTime = time
-      : __transitionTime = 500);
+      ? TRANSITION_TIME = time
+      : TRANSITION_TIME = 500);
   }
   /**
    * This is used to generalize the way the helper functions are used
@@ -37,7 +35,7 @@ export class DOMHelper {
       // parameter probably is a query string, query the DOM first
       return document.querySelector(el);
     }
-    else if (el instanceof 'Element') {
+    else if (el instanceof Element) {
       // parameter is an element, no further querying to do
       return el;
     }
@@ -58,61 +56,70 @@ export class DOMHelper {
    * The CSS class HAS TO BE defined in your stylesheets
    */
   static show(el) {
-    this.query.classList.remove('is-hidden');
+    DOMHelper.query(el).classList.remove('is-hidden');
   }
   /**
    * Hides an element by adding the 'is-hidden' class
    * The CSS class HAS TO BE defined in your stylesheets
    */
   static hide(el) {
-    this.helpers.query(el).classList.add('is-hidden');
+    DOMHelper.query(el).classList.add('is-hidden');
+  }
+  static click(el) {
+    let event = document.createEvent('HTMLEvents');
+    event.initEvent('click', true, false);
+    DOMHelper.query(el).dispatchEvent(event);
   }
   /**
    * Fades in a previously hidden element with CSS classes
    * @see DOMHelper.hide()
+   * @param {string|Element} el element to fade in
    */
   static fadeIn(el) {
-    let element = this.helpers.query(el);
+    let element = DOMHelper.query(el);
     element.classList.remove('is-hidden');
     element.classList.add('is-fading-in');
     setTimeout(() => {
       element.classList.remove('is-fading-in');
-    }, this.transitionTime);
+    }, DOMHelper.getTransitionTime());
   }
   /**
    * Fades out an element and adds the 'is-hidden' class afterwards
    * @see DOMHelper.hide()
+   * @param {string|Element} el element to fade out
    */
   static fadeOut(el) {
-    let element = this.helpers.query(el);
+    let element = DOMHelper.query(el);
     element.classList.add('is-fading-out');
     setTimeout(() => {
       element.classList.remove('is-fading-out');
       element.classList.add('is-hidden');
-    }, this.transitionTime);
+    }, DOMHelper.getTransitionTime());
   }
   /**
    * Slides down a previously hidden element with CSS classes
    * @see DOMHelper.hide()
+   * @param {string|Element} el element to slide down
    */
   static slideDown(el) {
-    let element = this.helpers.query(el);
+    let element = DOMHelper.query(el);
     element.classList.remove('is-hidden');
     element.classList.add('is-sliding-down');
     setTimeout(() => {
       element.classList.remove('is-sliding-down');
-    }, this.transitionTime);
+    }, DOMHelper.getTransitionTime());
   }
   /**
    * slides up an element and adds the 'is-hidden' class afterwards
    * @see DOMHelper.hide()
+   * @param {string|Element} el element to slide up
    */
   static slideUp(el) {
-    let element = this.helpers.query(el);
+    let element = DOMHelper.query(el);
     element.classList.add('is-sliding-up');
     setTimeout(() => {
       element.classList.remove('is-sliding-up');
       element.classList.add('is-hidden');
-    }, this.transitionTime);
+    }, DOMHelper.getTransitionTime());
   }
 }
