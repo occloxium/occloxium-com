b59d046406a0eb678a86c2c632d16735259949e0
Alexander Bartolomey <occloxium@gmail.com>
2019-09-04 23:58:52 +0200

    added some more content to seminar

--- client/src/views/case-study/Seminar.vue
+++ client/src/views/case-study/Seminar.vue
@@ -3,7 +3,7 @@
     <div class="header"></div>
     <div class="title">
       <h2>Case Study:</h2>
-      <h1><small>Seminar (Internet Technologies): </small>Building & Deploying LaTeX Documents</h1>
+      <h1><small>Seminar (Internet Technologies): </small>Building &amp; Deploying LaTeX Documents</h1>
     </div>
     <div class="body">
       <p>
@@ -20,6 +20,39 @@
         got out of hand really fast, disobeying all previously defined guidelines. Directory structure and inclusion of
         files in TeX is pretty broken anyways so this time, I wanted to get it right, or at least better.
       </p>
+      <p>
+        To make life easier for me, as I wanted my build versions of the paper to be available in a central place
+        other than my hard drive, where PDFs would probably have interfered, and my supervisor in a way that 
+        I could quickly get feedback on drafts without actually having to send the document via e-mail, again 
+        creating interference of versions on his side, I entered the planning phase for a deployment pipeline
+        with visual version control integrated using the web.
+      </p>
+    </div>
+    <div class="no-margin">
+      <div id="page2"></div>
+      <div class="label">
+        Schematics of the pipeline architecture 
+      </div>
+    </div>
+    <p>
+      Hours at the drawing board spawned the above architecture. Part One, the upper one, covers the actual production of PDF output. 
+      On push to the remote repository, a fairly common build pipeline for documents using the typesetting environment LaTeX runs on
+      the source files for the paper. LaTeX pipelines have been done before, however, using Docker to build documents seems to be not 
+      that widely spread, which is a shame, since the portability gained from doing so eliminates the problem of manually installing 
+      TeXLive or another TeX distribution, which are classically pretty stubborn.
+    </p>
+    <p>
+      Using a self-made docker image bundling the <code>latexmk</code> toolchain as well as <code>TikZ</code>, required for graphics and
+      the rest of the remaining classes, 3 different PDFs, differing in form and style, are produced and exported as artifacts of that pipeline.
+      The <a href="https://www.github.com/occloxium/docker-latexmk">docker image</a> is actually quite versatile, so I will link it here for 
+      interested readers. Using it is dead simple, just mount a volume with your source files and override the entrypoint/cmd with latexmk
+      to your files.
+    </p>
+    <p>
+      
+    </p>
+    <div class="no-margin">
+      <div id="page3"></div>
     </div>
   </article>
 </template>
@@ -91,5 +124,22 @@ $zoomoid-fade: linear-gradient(135deg,
 .title,
 .body {
   max-width: 1024px;
+}
+.no-margin {
+  box-sizing: border-box;
+  padding: 2em;
+  width: 100%;
+  div {
+    height: 360px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center center;
+  }
+}
+#page2 {
+  background-image: url('../../assets/case-study/seminar/page_2.svg');
+}
+#page3 {
+  background-image: url('../../assets/case-study/seminar/page_3.svg');
 }
 </style>
