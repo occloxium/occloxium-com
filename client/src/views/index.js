69233f02d057ca4dec433ec45bcd266ae2a28f97
Alexander Bartolomey <occloxium@gmail.com>
2019-09-27 08:51:45 +0200

    added temporary vorkurs site
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -2,10 +2,12 @@ import About from './About';
 import Intro from './Intro';
 import Home from './Home';
 import CaseStudy from './CaseStudy';
+import Vorkurs from './Vorkurs';
 
 export {
   Home,
   Intro,
   About,
   CaseStudy,
+  Vorkurs,
 };
